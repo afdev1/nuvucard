b21a564e6e5bad73ba89804a86cca339d8f22982
Andrés Felipe De Orcajo <afdev@unal.edu.co>
2020-12-05 15:00:58 -0500

    Proyecto base

---
+++ src/main/java/com/afdev1/nuvucard/NuvucardApplication.java
@@ -0,0 +1,13 @@
+package com.afdev1.nuvucard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuvucardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NuvucardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/afdev1/nuvucard/NuvucardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afdev1.nuvucard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuvucardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
